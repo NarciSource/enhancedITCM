110385206a2e189182f31c90fb1570a9ae7d66b5
narci nar <jwch11@gmail.com>
2019-05-04 17:19:29 +0900

    ver 0.1.7-3 Fix analog timer size

--- enhancedITCM.meta.js
+++ enhancedITCM.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         enhancedITCM
 // @namespace    etcm
-// @version      0.1.7-2
+// @version      0.1.7-3
 // @description  EnhancedITCM is a user script that enhances the http://itcm.co.kr/
 // @author       narci <jwch11@gmail.com>
 // @match        *://itcm.co.kr/*
